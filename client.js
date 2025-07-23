ceac5026233930fd8588b4c70e22e3fe3838a40e
pushpaarivumani7-a <pushpaarivumani7@gmail.com>
2025-07-23 13:26:45 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,23 @@
+const socket = io();
+
+// Send message
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+// Receive message
+socket.on('chat message', (msg) => {
+  const li = document.createElement('li');
+  li.textContent = msg;
+  messages.appendChild(li);
+  messages.scrollTop = messages.scrollHeight;
+});
+
